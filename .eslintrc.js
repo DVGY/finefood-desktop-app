0656bdf13680df9e1a850952ca1198ee33db2117
DVGY <gaurav.gydv@gmail.com>
2023-06-05 20:50:57 +0530

    add eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  plugins: ["react", "react-hooks", "import"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:no-console/recommended",
+  ],
+  rules: {
+    // Add any additional rules or overrides here
+    "no-console": "error", // Enforce error for console.log and related statements
+    "import/order": [
+      "error",
+      {
+        groups: [
+          ["builtin", "external"],
+          "internal",
+          ["parent", "sibling", "index"],
+        ],
+        "newlines-between": "always",
+      },
+    ],
+  },
+};
--- src/components/dashboard/DailyRevenueChart.tsx
+++ src/components/dashboard/DailyRevenueChart.tsx
@@ -1,4 +1,5 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery } from "@tanstack/react-query";
+import { ApiError } from "@common/components/error";
 import {
   LineChart,
   Line,
@@ -7,17 +8,16 @@ import {
   ResponsiveContainer,
   XAxis,
   Label,
-  Layer,
-} from 'recharts';
-import { Box, useMantineTheme } from '@mantine/core';
+} from "recharts";
+import { useMantineTheme } from "@mantine/core";
+import { DailyOrdersChart } from "./DailyOrdersChart";
+import { Loading } from "@common/components/loading";
 
-import { ApiError } from '@common/components/error';
-import { Loading } from '@common/components/loading';
-import { ResourceList } from '@common/route';
+import { ResourceList } from "@common/route";
 import {
   QueryStringParams,
   getFormattedQueryStringParams,
-} from '@common/react-query/utils';
+} from "@common/react-query/utils";
 
 type DailyRevenue = {
   date: string;
@@ -43,24 +43,24 @@ const fetchData = async (
 
   if (!response.ok) {
     if (response.status === 404) {
-      throw new Error('Data not found');
+      throw new Error("Data not found");
     } else {
-      throw new Error('Failed to fetch data');
+      throw new Error("Failed to fetch data");
     }
   }
   return response.json();
 };
 
 const DailyRevenuePresentation = () => {
-  const start = 'Sun, 28 May 2023 18:30:00 GMT';
-  const end = 'Sun, 04 Jun 2023 18:30:00 GMT';
+  const start = "Sun, 28 May 2023 18:30:00 GMT";
+  const end = "Sun, 04 Jun 2023 18:30:00 GMT";
   const queryStringParams = {
     start,
     end,
   };
 
   const { data, isLoading, isError } = useQuery(
-    ['dailyRevenue', queryStringParams],
+    ["dailyRevenue", queryStringParams],
     () => fetchData(queryStringParams)
   );
 
@@ -71,9 +71,10 @@ const DailyRevenuePresentation = () => {
   if (isError) {
     return <ApiError />;
   }
-
   return <DailyRevenueChartContainer data={data} />;
 };
+
+console.log("sdafads");
 
 const DailyRevenueChartContainer = ({
   data,
@@ -83,7 +84,7 @@ const DailyRevenueChartContainer = ({
   const theme = useMantineTheme();
 
   return (
-    <ResponsiveContainer width='100%' height='100%'>
+    <ResponsiveContainer width="100%" height="100%">
       <LineChart
         width={500}
         height={300}
@@ -95,58 +96,58 @@ const DailyRevenueChartContainer = ({
           bottom: 5,
         }}
       >
-        <CartesianGrid strokeDasharray='3 3' />
+        <CartesianGrid strokeDasharray="3 3" />
         <Tooltip
           formatter={(value) => `$ ${value}`}
-          labelFormatter={() => ''}
+          labelFormatter={() => ""}
           itemStyle={{ color: theme.colors.dark[9] }}
         />
         {/*    Create SVG Icon
          */}
         <g>
-          <g className='custom-p&l-number'>
+          <g className="custom-p&l-number">
             <text
-              x='15%'
-              y='10%'
+              x="15%"
+              y="10%"
               dy={+12}
               style={{
                 fontSize: 36,
-                fontWeight: 'bold',
+                fontWeight: "bold",
                 fill: theme.colors.green[7],
               }}
               width={200}
-              textAnchor='middle'
+              textAnchor="middle"
             >
               ${(data.total / 1000).toFixed(1)}K
             </text>
           </g>
-          <g className='custom-upward-trend-icon'>
+          <g className="custom-upward-trend-icon">
             <svg
-              viewBox='0 0 25 25'
+              viewBox="0 0 25 25"
               height={100}
               width={100}
-              x='20%'
-              y='0%'
-              fill='none'
-              xmlns='http://www.w3.org/2000/svg'
+              x="20%"
+              y="0%"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                d='M8 14.5L12.5 10L17 14.5'
+                d="M8 14.5L12.5 10L17 14.5"
                 stroke={theme.colors.green[5]}
-                stroke-width='1.5'
+                stroke-width="1.5"
               />
             </svg>
           </g>
         </g>
 
         <Line
-          type='monotone'
-          dataKey='value'
+          type="monotone"
+          dataKey="value"
           fill={theme.colors.green[4]}
           stroke={theme.colors.green[8]}
         />
-        <XAxis dataKey='name'>
-          <Label value='Daily Revenue' offset={0} position='insideBottom' />
+        <XAxis dataKey="name">
+          <Label value="Daily Revenue" offset={0} position="insideBottom" />
         </XAxis>
       </LineChart>
     </ResponsiveContainer>
